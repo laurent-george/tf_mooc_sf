2ebf133d62d7d779b33d6f8b4d69d33bd302a8a0
Laurent George <laurent.f.george@gmail.com>
2017-10-22 21:17:28 +0200

    Trying to improve speed of dataset

--- assignment_1/q2/q2_exercice.py
+++ assignment_1/q2/q2_exercice.py
@@ -11,6 +11,7 @@ from tensorflow.examples.tutorials.mnist import input_data
 from tensorflow.python.training import training_util
 from tensorflow.contrib.data import Dataset, TFRecordDataset
 
+from functools import partial
 import time
 
 tf.logging.set_verbosity(tf.logging.INFO)   # require to see the training hooks stuff
@@ -31,7 +32,8 @@ def perso_mnist_net_model_fn(features, labels, mode=tf.estimator.ModeKeys.TRAIN,
 
     n_classes = 10
 
-    X = features['feature']
+    #X = features['feature']
+    X = features
     labels = tf.reshape(labels, [-1, n_classes])  # ou tf.squeeze
     Y = tf.cast(labels, tf.int32)
     flat_input = tf.keras.layers.Flatten()(X)
@@ -123,7 +125,11 @@ def get_inputs_fn_based_on_generator():
     my_input_fn_train = generator_input_fn(lambda: input_fn(mnist.train, 30), target_key='label', batch_size=128)
     return my_input_fn_train, my_input_fn_val, my_input_fn_test
 
-def get_inputs_fn_based_on_tfrecords():
+def get_input_fn_dataset(dataset_name = 'train', num_epoch=30, batch_size=128):
+    # idea taken from https://stackoverflow.com/questions/45620449/initialization-of-tf-contrib-data-iterator-with-tf-estimator
+    # pas de vrai amelioration en terme de speed.. TODO: tester sans iterateur tout dans le meme graph
+    # in order to have everythings in same graph
+    init_hook = IteratorInitHook()
     def _parse_function(example_proto):
       features = {"image": tf.FixedLenFeature([784], tf.float32),
                   "label": tf.FixedLenFeature([10], tf.float32)}
@@ -131,24 +137,41 @@ def get_inputs_fn_based_on_tfrecords():
       parsed_features = tf.parse_example(example_proto, features)
       return parsed_features["image"], parsed_features["label"]
 
-    def input_fn(fname, num_epoch=1, batch_size=128):
-        dataset = TFRecordDataset(fname, compression_type='GZIP')
+    def input_fn():
+        dataset = TFRecordDataset('data/{}.tfrecords'.format(dataset_name), compression_type='GZIP')
         dataset = dataset.batch(batch_size)
-        #dataset = dataset.prefetch(10)
+        dataset = dataset.prefetch(10)
         dataset = dataset.repeat(num_epoch)
         dataset = dataset.map(_parse_function)
-        iterator = dataset.make_one_shot_iterator()
-        image, label = iterator.get_next()
 
-        return {'feature':image}, label
+        iterator = dataset.make_initializable_iterator()
+        init_hook.iterator_init_op = iterator.initializer
+        #iterator = dataset.make_one_shot_iterator()
+        return iterator.get_next()
+        #image, label = iterator.get_next()
+        #return image, label
+    return input_fn, init_hook
+    #return iterator.get_next()
 
-    input_fn_train = lambda: input_fn('data/train.tfrecords', 30)
-    input_fn_val = lambda: input_fn('data/validation.tfrecords', 1)
-    input_fn_test = lambda: input_fn('data/validation.tfrecords', 1)
-    return input_fn_train, input_fn_val, input_fn_test
+
+# attention using a lambda here.. you get something really slow..
+    #input_fn_train = lambda: input_fn('data/train.tfrecords', 30)
+    #input_fn_val = lambda: input_fn('data/validation.tfrecords', 1)
+    #input_fn_test = lambda: input_fn('data/validation.tfrecords', 1)
+
+    #input_fn_train = partial(input_fn, 'data/train.tfrecords', 30)
+    #input_fn_val = partial(input_fn, 'data/validation.tfrecords', 1)
+    #input_fn_test = partial(input_fn, 'data/validation.tfrecords', 1)
 
 
 
+
+
+
+class IteratorInitHook(tf.train.SessionRunHook):
+
+    def after_create_session(self, session, coord):
+        session.run(self.iterator_init_op)
 
 
 def main():
@@ -165,20 +188,25 @@ def main():
 
 
     if tf.app.flags.FLAGS.enable_tfrecords:
-        input_train, input_validation, input_test = get_inputs_fn_based_on_tfrecords()
+        input_train, input_train_init_hook = get_input_fn_dataset('train', 30)
+        input_validation, input_validation_init_hook = get_input_fn_dataset('validation', 1)
+        input_test, input_test_init_hook = get_input_fn_dataset('test', 1)
     else:
         input_train, input_validation, input_test = get_inputs_fn_based_on_generator()
 
     print("Test")
-    #estimator.train(input_fn=input_train)
+    estimator.train(input_fn=input_train, hooks=[input_train_init_hook])
 
-    print("Bench iterator speed")
-    session = tf.Session()
-    while True:
-        start = time.time()
-        session.run(input_train())
-        duration = time.time() - start
-        print("duration is {}".format(duration))
+    #print("Bench iterator speed")
+    #session = tf.Session()
+    #session.run(input_train_init_hook.iterator_init_op)
+    #while True:
+    #    start = time.time()
+    #    val = session.run(input_train())  # <-- the duration never stop to increase here
+    #    #print(val)
+
+    #    duration = time.time() - start
+    #    print("duration is {}".format(duration))
 
 
 if __name__ == "__main__":
